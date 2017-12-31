b786e9683f90acf72b9557e69347bbfd32d3d798
Adam Chomicki <admchm@outlook.com>
2017-12-31 23:27:30 +0100

    Update grabber

--- RoomEscape/Source/RoomEscape/ItemsGrabber.cpp
+++ RoomEscape/Source/RoomEscape/ItemsGrabber.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ItemsGrabber.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UItemsGrabber::UItemsGrabber()
@@ -18,9 +18,8 @@ UItemsGrabber::UItemsGrabber()
 void UItemsGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 	
+	UE_LOG(LogTemp, Warning, TEXT("Grabber loaded"));
 }
 
 
